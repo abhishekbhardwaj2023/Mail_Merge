239b2655e7a7a8c1cbf5efe93f49169652e1db1c
ABHISHEK BHARDWAJ <59165472+abhishekbhardwaj2023@users.noreply.github.com>
2021-12-04 00:49:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+# Global Variable
 PLACEHOLDER = "[name]"
 
-
+# Open file and read content
 with open("D:/VS_Code Programs/Python/Mail_Merge_Project/Input/Names/invited_names.txt") as names_file:
     names = names_file.readlines()
 
+# Merging names with the invitation letter
 with open("D:/VS_Code Programs/Python/Mail_Merge_Project/Input/Letters/starting_letter.txt") as letter_file:
     letter_contents = letter_file.read()
     for name in names:
